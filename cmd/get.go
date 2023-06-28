f0bb7bcd70d569b4bba03aaa607426ac68cc9b74
Bruno Belarte <bruno.belarte@gmail.com>
2023-06-28 17:18:47 +0200

    add first command

---
+++ cmd/get.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get actor details",
+	Long:  `Get actor details`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("get called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/belarte/scrap6degrees/cmd"
+
+func main() {
+	cmd.Execute()
+}
